ae472fd0c5d34da2a775d1fdb3a55328befd451b
nerfiko <nerfiko.official@gmail.com>
2022-01-16 20:30:09 +0100

    footer, pages

--- src/app.js
+++ src/app.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import jumboData from './fixtures/jumbo';
-import Jumbotron from './components/jumbotron';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, Browse, SignIn, SignUp } from './pages';
+import * as ROUTES from './constants/routes';
 
 export default function App() {
   return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt} />
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
+    <Home />
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { HeaderContainer } from '../containers/header';
+import { FaqsContainer } from '../containers/faqs';
+import { FooterContainer } from '../containers/footer';
+import { JumbotronContainer } from '../containers/jumbotron';
+
+export default function Home(){
+    return (
+        <>
+            <HeaderContainer>
+                <JumbotronContainer /> 
+                <FaqsContainer />
+                <FooterContainer /> 
+            </HeaderContainer>
+        </>
+    );
+}
