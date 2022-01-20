53d81d69e33a667aa98725d28497028495f0a4e9
nerfiko <nerfiko.official@gmail.com>
2022-01-20 13:40:31 +0100

    browse page

--- src/app.js
+++ src/app.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Switch,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 import { useAuthListener } from './hooks';
 
 export default function App() {
-  const user = useAuthListener();
-  console.log(user);
+  const { user } = useAuthListener();
+
   return (
     <Router>
       <Switch>
--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -30,6 +30,7 @@ export const Logo = styled.img`
     height: 32px;
     width: 108px;
     margin-right: 40px;
+    margin-top: 40px;
 
     @media (min-width: 1449px){
         height: 45px;
